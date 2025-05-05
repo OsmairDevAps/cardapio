b485e69f3059a9143fded3fc194f4c98bca35c75
OsmairDevAps <osmairdev@gmail.com>
2025-05-05 12:00:34 -0300

    Up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import imgcapa from '../assets/capamenu.png'
 
 export default function Home() {
+
   return (
-    <div>
-      <h1>Cardápio</h1>
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <Image src={imgcapa} width={700} height={700} alt="Cardápio Tio do Crepe" />
+      <div className="flex flex-row justify-center items-center gap-4 mt-4">
+        <div>PREV</div>
+        <div>NEXT</div>
+      </div>
     </div>
   );
 }
